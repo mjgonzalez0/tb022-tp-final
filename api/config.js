454f5fc6c8d62291c0f90cef86fd168422d50c6b
mjgonzalez0 <mjgonzalez@fi.uba.ar>
2025-11-13 16:44:21 -0300

    fix misspelled variable

--- api/config.js
+++ api/config.js
@@ -5,7 +5,7 @@ loadEnvFile();
 export const CONFIG = {
   port: process.env.PORT,
   database: {
-    username: process.env.DATABASE_USERNAME,
+    username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     name: process.env.DATABASE_NAME,
   }
