53e8279251285d7f3414a2a542933149611f7955
Fernando Cualchi linux <fcualchi@fi.uba.ar>
2026-01-27 23:40:21 -0300

    Mensaje de correo y usuario cuando estan vacios

--- web/registro.js
+++ web/registro.js
@@ -1,15 +1,34 @@
-const form = document.getElementById("formulario");
-const password = document.getElementById("contraseña");
-const confirmPassword = document.getElementById("confirmarcontraseña");
-const passwordNotMatch = document.getElementById("errorcontraseña1");
-const passwordNotMatch2  = document.getElementById("errorcontraseña2");
-const passwordLength = document.getElementById("largoContraseña")
 const formulario = document.getElementById("formulario");
 
-form.addEventListener("submit", (evento) => {
-    if(!form.checkValidity()){
-        evento.preventDefault(); 
-        return;
+const password = document.getElementById("contraseña");
+const confirmPassword = document.getElementById("confirmarcontraseña");
+
+const passwordNotMatch = document.getElementById("errorcontraseña1");
+const passwordNotMatch2  = document.getElementById("errorcontraseña2");
+
+const passwordLength = document.getElementById("largoContraseña")
+
+const emptyUser = document.getElementById("usuario");
+const emptyCorreo = document.getElementById("correo");
+
+const mensajeSinUser = document.getElementById("sin-usuario");
+const mensajeSinCorreo = document.getElementById("sin-correo");
+
+
+formulario.addEventListener("submit", (evento) => {
+    mensajeSinUser.textContent = "";
+    mensajeSinCorreo.textContent = "";
+    if(!formulario.checkValidity()){
+        evento.preventDefault();
+        if (emptyUser.value === ""){
+            mensajeSinUser.textContent = "Es obligatorio colocar un usuario"
+        }
+        if (emptyCorreo.value === ""){
+            mensajeSinCorreo.textContent = "Es obligatorio colocar un correo"
+        }
+        
+    }else {
+
     }
 });
 
