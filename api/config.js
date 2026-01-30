003dabbecb0bacd399431cf202c73d30007bc985
mjgonzalez0 <mjgonzalez@fi.uba.ar>
2026-01-30 11:55:52 -0300

    fix database hostname causing api to not start

--- api/config.js
+++ api/config.js
@@ -9,6 +9,7 @@ try {
 export const CONFIG = {
   port: process.env.API_PORT,
   database: {
+    host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     name: process.env.DATABASE_NAME,
--- api/db.js
+++ api/db.js
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 import { CONFIG } from "./config.js"
 
 export const pool = new Pool({
+  host: CONFIG.database.host,
   user: CONFIG.database.username,
   database: CONFIG.database.name,
   password: CONFIG.database.password,
